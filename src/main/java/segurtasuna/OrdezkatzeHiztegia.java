224009747e0949d9885fc0c1af5baf8a928866d5
josuf6 <josuf6@gmail.com>
2020-09-29 15:50:49 +0200

    OrdezkatzeHiztegia eta OrdezkatzeHiztegia klaseak sortu

---
+++ src/main/java/segurtasuna/OrdezkatzeHiztegia.java
@@ -0,0 +1,14 @@
+package segurtasuna;
+
+public class OrdezkatzeHiztegia {
+
+    private String gakoa = "ZXCVBNMASDFGHJKLQWERTYUIOP";
+
+    public String zifratu(String mezua){
+
+    }
+
+    public String deszifratu(String kripto){
+
+    }
+}
